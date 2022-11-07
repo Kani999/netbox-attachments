ecd0af3d46f3cbce4e2317cd50ced2c1673c1973
Jan Krupa <jan.krupa@cesnet.cz>
2022-11-07 12:21:15 +0000

    Define filtersets for attachments

---
+++ netbox_attachments/filtersets.py
@@ -0,0 +1,23 @@
+import django_filters
+from netbox.filtersets import BaseFilterSet
+from utilities.filters import ContentTypeFilter
+
+from .models import NetBoxAttachment
+
+
+class InvMonFileAttachmentFilterSet(BaseFilterSet):
+    q = django_filters.CharFilter(
+        method='search',
+        label='Search',
+    )
+    created = django_filters.DateTimeFilter()
+    content_type = ContentTypeFilter()
+
+    class Meta:
+        model = NetBoxAttachment
+        fields = ['id', 'content_type_id', 'object_id', 'name']
+
+    def search(self, queryset, name, value):
+        if not value.strip():
+            return queryset
+        return queryset.filter(name__icontains=value)
