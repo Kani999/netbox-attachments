fdc3f36f1a8f651e1a75c0f555701a28eca5602a
Jan Krupa <jan.krupa@cesnet.cz>
2022-11-07 13:46:24 +0000

    Render attachments panel to all models on the right

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # netbox-attachments
 
 Manage attachments for any model in NetBox
+
+### Configuration
+- `configuration.py`
+```
+# Add plugin
+PLUGINS = ['netbox_attachments']
+# Set to which app (and models under it) could be attachments stored
+PLUGINS_CONFIG = {
+    'netbox_attachments': {
+        'apps': ['dcim', 'ipam', 'circuits', 'tenancy', 'virtualization', 'wireless', '<custom_plugin>'],
+    }
+}
+```
+
+- Caveats
+    - If you want to enable appending attachments to your plugin, you have to add some code to your plugin codebase
+    - Extending your views at `templates/<plugin>/<detail_view>.html`
+    - ```
+        # At the TOP
+        {% load plugins %}
+        ...
+        # Then under the comments section 
+        {% plugin_right_page object %}
+      ```
+    - It's same as for the core models e.g.
+        - https://github.com/netbox-community/netbox/blob/c1b7f09530f0293d0f053b8930539b1d174cd03b/netbox/templates/dcim/device.html#L288
---
+++ netbox_attachments/template_content.py
@@ -0,0 +1,34 @@
+from django.contrib.contenttypes.models import ContentType
+from django.conf import settings
+from extras.plugins import PluginTemplateExtension
+from django.apps import apps
+from .models import NetBoxAttachment
+
+plugin_settings = settings.PLUGINS_CONFIG.get('netbox_attachments', {})
+available_apps = plugin_settings.get("apps", [
+                                     'dcim', 'ipam', 'circuits', 'tenancy', 'virtualization', 'wireless'])
+
+template_extensions = []
+
+
+def right_page(self):
+    obj = self.context['object']
+    return self.render(
+        'netbox_attachments/netbox_attachment_panel.html',
+        extra_context={
+            'files': NetBoxAttachment.objects.filter(content_type=ContentType.objects.get(app_label=self.model.split(".")[0], model=self.model.split(".")[1]).id, object_id=obj.id),
+        }
+    )
+
+
+# Generate plugin extension for all classes
+for app_label, classes in apps.all_models.items():
+    if app_label in available_apps:
+        for model_name, model_class in classes.items():
+            name = model_name + "_plugin_template_extension"
+            dynamic_klass = type(name,
+                                 (PluginTemplateExtension, ),
+                                 {"model": f"{app_label}.{model_name}",
+                                     "right_page": right_page})
+
+            template_extensions.append(dynamic_klass)
