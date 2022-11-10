5b1b36f0af36e1f68ce045d1e6f735a470388fae
Jan Krupa <jan.krupa@cesnet.cz>
2022-11-10 09:32:53 +0000

    Enable configuring preferred display of attachments

--- netbox_attachments/template_content.py
+++ netbox_attachments/template_content.py
@@ -23,17 +23,38 @@ def attachments_panel(self):
     )
 
 
+def get_display_on(app_model_name):
+    """Get prefered display setting (left_page, right_page, full_width_page) for attachment panel
+
+    Args:
+        app_model_name (str): <app_label>.<model> = (dcim.device, ipam.vlan, ...)
+
+    Returns:
+        str: Configured display setting or default
+    """
+    # set default
+    display_on = plugin_settings.get("display_default", "right_page")
+
+    # Find configured display setting or return default
+    if display_setting := plugin_settings.get('display_setting'):
+        display_on = display_setting.get(app_model_name, display_on)
+
+    return display_on
+
+
 # Generate plugin extension for all classes
 for content_type in ContentType.objects.all():
     app_label = content_type.app_label
     model = content_type.model
-    display_default = plugin_settings.get("display_default", "right_page")
+    app_model_name = f"{app_label}.{model}"
 
     if app_label in plugin_settings.get("apps"):
         klass_name = f"{app_label}_{model}_plugin_template_extension"
+
         dynamic_klass = type(klass_name,
-                             (PluginTemplateExtension, ),
-                             {"model": f"{app_label}.{model}",
-                              display_default: attachments_panel})
+                             (PluginTemplateExtension,),
+                             {"model": app_model_name,
+                              get_display_on(app_model_name): attachments_panel}
+                             )
 
         template_extensions.append(dynamic_klass)
