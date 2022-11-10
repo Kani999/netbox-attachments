030cac80652c2a3db4440232c0feb52742f9f483
Jan Krupa <jan.krupa@cesnet.cz>
2022-11-10 08:17:21 +0000

    Refactor class generating for template extension

--- netbox_attachments/template_content.py
+++ netbox_attachments/template_content.py
@@ -1,31 +1,38 @@
-from django.contrib.contenttypes.models import ContentType
 from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
 from extras.plugins import PluginTemplateExtension
-from django.apps import apps
-from .models import NetBoxAttachment
 
+from .models import NetBoxAttachment
 
 plugin_settings = settings.PLUGINS_CONFIG.get('netbox_attachments', {})
 template_extensions = []
 
+
 def right_page(self):
     obj = self.context['object']
+    app_label, model = self.model.split(".")
+    content_type_id = ContentType.objects.get(app_label=app_label,
+                                              model=model).id
+
     return self.render(
         'netbox_attachments/netbox_attachment_panel.html',
         extra_context={
-            'files': NetBoxAttachment.objects.filter(content_type=ContentType.objects.get(app_label=self.model.split(".")[0], model=self.model.split(".")[1]).id, object_id=obj.id),
+            'netbox_attachments': NetBoxAttachment.objects.filter(content_type_id=content_type_id,
+                                                                  object_id=obj.id),
         }
     )
 
 
 # Generate plugin extension for all classes
-for app_label, classes in apps.all_models.items():
-    if app_label in plugin_settings.get("apps"):
-        for model_name, model_class in classes.items():
-            name = model_name + "_plugin_template_extension"
-            dynamic_klass = type(name,
-                                 (PluginTemplateExtension, ),
-                                 {"model": f"{app_label}.{model_name}",
-                                     "right_page": right_page})
+for content_type in ContentType.objects.all():
+    app_label = content_type.app_label
+    model = content_type.model
 
-            template_extensions.append(dynamic_klass)
+    if app_label in plugin_settings.get("apps"):
+        klass_name = f"{app_label}_{model}_plugin_template_extension"
+        dynamic_klass = type(klass_name,
+                             (PluginTemplateExtension, ),
+                             {"model": f"{app_label}.{model}",
+                                 "right_page": right_page})
+
+        template_extensions.append(dynamic_klass)
