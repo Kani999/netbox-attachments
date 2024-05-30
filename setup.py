818ce2d501b151b1c7a8d36e324470812ddd8068
Jan Krupa <jan.krupa@cesnet.cz>
2024-05-30 12:51:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,19 +26,17 @@ setup(
     long_description_content_type="text/markdown",
     url='https://github.com/Kani999/netbox-attachments',
     author="Jan Krupa",
-    #author_email='your.email@example.com',  # Add your email here
-    license='MIT',  # Specify the license
+    license='Apache 2',
     install_requires=[],
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     python_requires='>=3.9',
     classifiers=[
-        'Development Status :: 4 - Beta',  # Update as necessary
         'Intended Audience :: Developers',
         'Framework :: Django',
-        'Framework :: Django :: 3.2',  # Specify the Django version if known
-        'License :: OSI Approved :: MIT License',
+        'Framework :: Django :: 5.0',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
@@ -46,4 +44,3 @@ setup(
     ],
     keywords=['netbox', 'netbox-plugin'],
 )
-
