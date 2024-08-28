59d01f0023e9ea1ab67b6c22a3f97e2b8e7d6a3e
Jan Krupa <jan.krupa@cesnet.cz>
2024-08-28 11:18:32 +0000

    Add management command to delete orphaned netbox attachments

---
+++ netbox_attachments/management/commands/remove_orphaned_netbox_attachments.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+import six.moves
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from netbox_attachments.management.cleanup import (
+    get_unused_media,
+    ATTACHMENT_MEDIA_ROOT,
+)
+
+
+class Command(BaseCommand):
+    help = "Clean unused media files which have no reference in models"
+
+    # verbosity
+    # 0 means silent
+    # 1 means normal output (default).
+    # 2 means verbose output
+
+    verbosity = 1
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--noinput",
+            "--no-input",
+            dest="interactive",
+            action="store_false",
+            default=True,
+            help="Do not ask confirmation",
+        )
+
+        parser.add_argument(
+            "-e",
+            "--exclude",
+            dest="exclude",
+            action="append",
+            default=[],
+            help="Exclude files by mask (only * is supported), can use multiple --exclude",
+        )
+
+        parser.add_argument(
+            "--minimum-file-age",
+            dest="minimum_file_age",
+            default=60,
+            type=int,
+            help="Skip files younger this age (sec) - default 60 sec",
+        )
+
+        parser.add_argument(
+            "-n",
+            "--dry-run",
+            dest="dry_run",
+            action="store_true",
+            default=False,
+            help="Dry run without any affect on your data",
+        )
+
+    def info(self, message):
+        if self.verbosity > 0:
+            self.stdout.write(message)
+
+    def debug(self, message):
+        if self.verbosity > 1:
+            self.stdout.write(message)
+
+    def _show_files_to_delete(self, unused_media):
+        self.debug("Files to remove:")
+
+        for f in unused_media:
+            self.debug(f)
+
+        self.info("Total files will be removed: {}".format(len(unused_media)))
+
+    def handle(self, *args, **options):
+        if "verbosity" in options:
+            self.verbosity = options["verbosity"]
+
+        self.info("Scanned folder: {}".format(ATTACHMENT_MEDIA_ROOT))
+
+        unused_media = get_unused_media(
+            exclude=options.get("exclude"),
+            minimum_file_age=options.get("minimum_file_age"),
+        )
+
+        if not unused_media:
+            self.info("Nothing to delete. Exit")
+            return
+
+        if options.get("dry_run"):
+            self._show_files_to_delete(unused_media)
+            self.info("Dry run. Exit.")
+            return
+
+        if options.get("interactive"):
+            self._show_files_to_delete(unused_media)
+
+            # ask user
+
+            question = "Are you sure you want to remove {} unused files? (y/N)".format(
+                len(unused_media)
+            )
+
+            if six.moves.input(question).upper() != "Y":
+                self.info("Interrupted by user. Exit.")
+                return
+
+        for f in unused_media:
+            self.debug("Remove %s" % f)
+            os.remove(os.path.join(settings.MEDIA_ROOT, f))
+
+        self.info("Done. Total files removed: {}".format(len(unused_media)))
