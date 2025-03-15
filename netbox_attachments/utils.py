37928709f634a8ce9765147a6263a5eb7b0344ec
Omri Abu <6192223+Omripresent@users.noreply.github.com>
2025-03-15 09:47:20 -0400

    Move model validation logic to utils Add validation checks to model save function

--- netbox_attachments/utils.py
+++ netbox_attachments/utils.py
@@ -1,5 +1,17 @@
 from pathlib import Path
 
+from django.conf import settings
+
+PLUGIN_SETTINGS = settings.PLUGINS_CONFIG.get("netbox_attachments", {})
+
+def choice_default(value, choices, default=None):
+    """
+    Return value only if present in choices, else return default value
+    """
+    if value not in choices:
+        return default
+    return value
+
 
 def attachment_upload(instance, filename):
     """
@@ -15,3 +27,20 @@ def attachment_upload(instance, filename):
     return "{}{}_{}_{}".format(
         path, instance.object_type.name, instance.object_id, filename
     )
+
+
+def validate_object_type(model):
+    """
+    Check if model is allowed to have attachments by checking plugin settings
+    """
+    applied_scope = choice_default(PLUGIN_SETTINGS.get("applied_scope"), ('app', 'model', ), 'app')
+    scope_filter = PLUGIN_SETTINGS.get("scope_filter", [])
+    model_match = model._meta.app_label
+
+    if applied_scope == 'app':
+        return model_match in scope_filter
+    elif applied_scope == 'model':
+        model_match = f"{model_match}.{model._meta.model_name}"
+        return model_match in scope_filter
+
+    return False
